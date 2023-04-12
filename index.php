7ff18900f9772e3f2895ed12c24fad00b5efedad
meyde26 <mehdi.kechiche26@gmail.com>
2023-04-12 15:50:15 +0200

    test

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="img/logo.jpg" alt="logo_site" class="logo"></a> 
+        <nav class="">
+            <ul class="nav_links">
+                <li><a href="">Projet</a></li>
+                <li><a href="">Equipes</a></li>
+                <li><a href="">Histoire</a></li>
+            </ul>
+        </nav>
+
+   </header>
+</body>
+</html>
